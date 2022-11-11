0e5f19fc84004e50806dfb787044407c49108053
Jasonn5 <98439093+Jasonn5@users.noreply.github.com>
2022-11-11 12:02:32 -0400

    Input and output in Java v2

--- javatraining/src/HighschoolTrainee4.java
+++ javatraining/src/HighschoolTrainee4.java
@@ -3,12 +3,16 @@ public class HighschoolTrainee4 {
     public static void main(String[] args){
         int StudentAge = 22;
         double Prom = 8.5;
+        String StudentName = "Jeyson Erik";
+        String LastName = "Valdivia Bernal";
         char FirstInitial = 'J';
         char LastInitial = 'V';
         boolean Attendance = true;
 
         System.out.println ("The Student Age is " + StudentAge) ;
         System.out.println ("The Prom is " + Prom);
+        System.out.println ("The StudentName is " + StudentName);
+        System.out.println ("The LastName is " + LastName);
         System.out.println ("The First Initial is " + FirstInitial);
         System.out. println ("The last Initial is "+ LastInitial) ;
         System.out.println ("The attendance is " + Attendance) ;
@@ -18,6 +22,13 @@ public class HighschoolTrainee4 {
         Scanner input = new Scanner(System.in);
         Prom = input.nextDouble();
 
+
+        System.out.println("Enter the new last Name :");
+
+        Scanner input2 = new Scanner(System.in);
+        LastName = input2.nextLine();
+
         System.out.println("The new prom are " + Prom);
+        System.out.println("The new last name is " + LastName);
     }
 }
